9cdd5ca1552438289af7441a0a8c885cca2ceb92
elli95 <39590842+elli95@users.noreply.github.com>
2023-03-24 06:55:05 +0100

    shopping cart fix

--- js/product-information.js
+++ js/product-information.js
@@ -99,9 +99,10 @@ function loadProduct(){
                                             <div class="error-message-style" id="size-error">Please select a size.</div>
                                             </div>
                                             <div class="grid-f product-button">
+                                            <div id="add-to-rart-reaction"></div>
                                             <div class="add-to-cart-button">
                                             <button class="button button-confirm" id="cart-button">Add to cart</button>
-                                            <a href="shopping-cart.html" class="button button-confirm">View Shopping Cart</a>
+                                            <a href="shopping-cart.html" class="button button-confirm" id="view-shopping-cart-btn">View Shopping Cart</a>
                                             </div>
                                             </div>  
                                             </div>
@@ -164,51 +165,265 @@ shoppingCartUpdate();
 
 addToCartButton.addEventListener("click", addToShoppingCart);
 addToCartButton.addEventListener("click", productFormValidator);
+addToCartButton.addEventListener("click", addToCartButtonReaction);
+addToCartButton.addEventListener("click", shoppingCartAmount);
 
 const colorError = document.querySelector("#color-error");
 const sizeError = document.querySelector("#size-error");
 
+const addToCartReaction = document.querySelector("#add-to-rart-reaction");
+
+function addToCartButtonReaction(){
+    let result = jacketList.filter(jacketName => jacketName.name === productName);
+    Object.values(result).forEach(function(product) {
+        addToCartReaction.innerHTML = `<p>${product.name} has been added to the shopping cart!</p>`;
+    });
+}
+
 function productFormValidator(){
     if(color.length) {
         colorError.style.display = "none"
-        console.log("hello");
     }
     else {
         colorError.style.display = "block"
-        console.log("its me");
     }
 
     if(size.length) {
         sizeError.style.display = "none";
-        console.log("hello again");
     }
     else {
         sizeError.style.display = "block";
-        console.log("its me again");
     }
 }
 
-function addToShoppingCart(){
-    if(shoppingCart.some((jacket) => jacket.name === productName)){
-        alert("Jacket is alredy in the cart. To change the product amount, please go to the shopping cart.")
-    } else{
-    const jacket = jacketList.find((jacketList) => jacketList.name === productName);
-    
+function addToShoppingCart(){ 
     let lastcolor = color[color.length - 1];
     let lastsize = size[size.length - 1];
 
-    shoppingCart.push({...jacket, color: lastcolor.color, size: lastsize.size, productAmount: 1});
-    console.log(shoppingCart);
-    } 
-    // else{
-    //     alert("Please select a color and size before adding the product to the basket.")
-    // }
+    let jacketName = shoppingCart.filter(jacketName => jacketName.name === productName);
+    let jacketType = jacketName.filter(jacketName => jacketName.size === lastsize.size && jacketName.color === lastcolor.color);
+
+    if(jacketType.length > 0){
+        alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+        
+    }
+    else {
+        const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+                
+        shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+    }
     shoppingCartUpdate();
 }
+
+
 
 function shoppingCartUpdate(){
     localStorage.setItem("SHOPPING CART", JSON.stringify(shoppingCart));
 }
+
+function shoppingCartAmount(){
+    const shoppingCartBtn = document.querySelector("#view-shopping-cart-btn")
+    console.log(shoppingCart.length);
+
+    if(shoppingCart.length === 0){
+        shoppingCartBtn.style.display = "none"
+    } else {
+        shoppingCartBtn.style.display = "flex"
+    }
+}
+shoppingCartAmount()
+
+
+    // let jacketType = jacketList.filter(jacketName => jacketName.name === productName);
+    // console.log("jacketType",jacketType);
+    // console.log("result",productName);
+    // console.log("jacketType--",jacketType.name === productName);
+    // const jacketNameInCart = shoppingCart.find((jacketItem) => jacketItem.name === productName);
+    // console.log("jacketNameInCart",jacketNameInCart);
+    // const shoppingCartValue = shoppingCart.length;
+    // console.log("jacketNameInCart",shoppingCartValue);
+    // console.log("shoppingCart",shoppingCart);
+
+    // console.log("length",shoppingCart.length === 0);
+    
+    // console.log("length",jacketNameInCart.name === productName);
+    
+    // console.log("length",jacketNameInCart.color !== lastcolor.color);
+    
+    // console.log("length",jacketNameInCart.size !== lastsize.size);
+
+    // let a = jacketNameInCart.name === productName;
+    // console.log("a",a);
+    // let b = jacketNameInCart.color !== lastcolor.color;
+    // console.log("b",b);
+    // let c = jacketNameInCart.size !== lastsize.size;
+    // console.log("c",c);
+
+    // Object.values(shoppingCart).forEach(function(product) {
+    //     // console.log("product",product);
+    //     // const jacketNameInCart = shoppingCart.find((jacketItem) => jacketItem.name === productName);
+    //     // console.log("jacketNameInCart",jacketNameInCart);
+    //     console.log("product",product);
+    //     // console.log("product--result",jacketType.name === product.name);
+    //     // // console.log("product--result",jacketType.name);
+    //     // const jacketNameInCart = shoppingCart.find((jacketItem) => jacketItem.name === product.name);
+    //     // console.log("jacketNameInCart",jacketNameInCart);
+
+// for(let i = 0; i < jacketType.length; i++){
+        //     console.log("shoppingCart", jacketType);
+        //     console.log("shoppingCart[i]",jacketType[i]);
+        // }
+            // let jacketColor = jacketType.filter(jacketName => jacketName.color === lastcolor.color);
+            // console.log("--------jacketColor",jacketColor);
+
+            // let jacketSize = jacketType.filter(jacketName => jacketName.size === lastsize.size);
+            // console.log("--------jacketSize",jacketSize);
+
+            
+            // let jacketColorSize = jacketType.color === jacketColor && jacketName.size === jacketSize;
+            // console.log("--------jacketColorSize",jacketColorSize);
+            
+            // let jacketC = jacketType.color;
+            // console.log("--------jacketC",jacketC);
+
+            // if((jacketType[i].name === productName && jacketType[i].color !== lastcolor.color && jacketType[i].size !== lastsize.size) || 
+            // (jacketType[i].name === productName && jacketType[i].color === lastcolor.color && jacketType[i].size !== lastsize.size) ||
+            // (jacketType[i].name === productName && jacketType[i].color !== lastcolor.color && jacketType[i].size === lastsize.size)){
+            // // if((jacketType[i].name === productName && jacketType[i].color !== lastcolor.color && jacketType[i].size !== lastsize.size) || 
+            // //    (jacketType[i].name === productName && jacketType[i].color === lastcolor.color && jacketType[i].size !== lastsize.size) ||
+            // //    (jacketType[i].name === productName && jacketType[i].color !== lastcolor.color && jacketType[i].size === lastsize.size)){
+                
+            //     const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+            //     console.log("-------------------------------------------------------",jacketItems);
+                        
+            //     // shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+            //     console.log("----breakbreakbreak----",shoppingCart);
+            //     // break;
+            //     // return;
+            // }
+            // else {
+        
+            //         console.log("----break----",shoppingCart);
+                    
+            //         // alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+            //         break;
+            
+            //     }
+
+
+            // let name = shoppingCart[i].name === productName;
+            // console.log("name",name);
+            // let color = shoppingCart[i].color === lastcolor.color;
+            // console.log("color",color);
+            // let size = shoppingCart[i].size === lastsize.size;
+            // console.log("size",size);
+
+            
+            // const jacketNameInCart = shoppingCart.find((jacketItem) => jacketItem.name === productName);
+            // console.log("jacketNameInCart",jacketNameInCart);
+            
+            // let jacketType = shoppingCart.filter(jacketName => jacketName.name === productName);
+            // console.log("jacketType",jacketType.size);
+            // console.log("jacketNameInCart--Test",jacketType.size === lastsize.size);
+            // console.log("amount",shoppingCart[i].name !== productName && 0 < (shoppingCart[i].name === productName));
+            // console.log("amount++",shoppingCart[i].name === productName);
+            // let color2 = shoppingCart[i].color === shoppingCart[i].color;
+            // console.log("color2",color2);
+            // let size2 = shoppingCart[i].size === shoppingCart[i].size;
+            // console.log("size2",size2);
+        
+        //     console.log("productName",product.name === productName);
+        // if((jacketNameInCart.name === productName && jacketNameInCart.color !== lastcolor.color && jacketNameInCart.size !== lastsize.size) || 
+        //    (shoppingCart[i].name === productName && shoppingCart[i].color === lastcolor.color && shoppingCart[i].size !== lastsize.size) ||
+        //    (shoppingCart[i].name === productName && shoppingCart[i].color !== lastcolor.color && shoppingCart[i].size === lastsize.size)){
+            
+        //     const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+        //     console.log("-------------------------------------------------------",jacketItems);
+                    
+        //     // shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+        //     console.log("----breakbreakbreak----",shoppingCart);
+        //     // break;
+        //     return;
+        // }
+        // // else if(shoppingCart[i].name === productName && shoppingCart[i].color === lastcolor.color && shoppingCart[i].size === lastsize.size) {
+        // else if(shoppingCart[i].name === productName){
+
+        //     console.log("----break----",shoppingCart);
+            
+        //     alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+        //     break;
+    
+        // }
+            // if(shoppingCart[i].name === productName && shoppingCart[i].color === lastcolor.color && shoppingCart[i].size === lastsize.size){
+            //     console.log("----break----",shoppingCart);
+                
+            //     alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+            //     break;
+            // }
+            // else if(shoppingCart[i].name === productName && (shoppingCart[i].color !== lastcolor.color || shoppingCart[i].size !== lastsize.size)) {
+            //     const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+            //     console.log("-------------------------------------------------------",jacketItems);
+                        
+            //     shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+            //     console.log("----breakbreakbreak----",shoppingCart);
+            //     break;
+        
+            // }
+        // }
+
+    // console.log("123131231233213",productName !== jacketNameInCart.name);
+    // Object.values(shoppingCart).forEach(function(product) {
+    
+    //     console.log("productName",product.name === productName);
+        
+    // if(shoppingCart.length === 0 || product.name !== productName || product.name === productName && (product.color !== lastcolor.color || product.size !== lastsize.size)){
+    //     // if(shoppingCart.length === 0 ||   (jacketType.name === productName && (jacketNameInCart.color !== lastcolor.color || jacketNameInCart.size !== lastsize.size))){
+
+    //      const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+    //      console.log("jacketItems",jacketItems);
+            
+    //     // shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+    //      console.log("-------",shoppingCart);
+         
+    //      console.log("++++",shoppingCart);
+    // }
+    // else {
+    //     alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+    // }
+// });
+    // if(!shoppingCartValue || (jacketNameInCart.name === productName && jacketNameInCart.color === lastcolor.color && jacketNameInCart.size === lastsize.size)){
+
+    //     alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+    // }
+    // else {
+    //     const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+            
+    //     shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+    //     console.log(shoppingCart);
+    // }
+
+// function addToShoppingCart(){
+    
+//     let lastcolor = color[color.length - 1];
+//     let lastsize = size[size.length - 1];
+//     console.log("123123123131231312312312312312312313212",shoppingCart);
+
+//     const jacketNameInCart = shoppingCart.find((jacketItem) => jacketItem.name === productName);
+//     console.log("123123123131231312312312312312312313212",jacketNameInCart);
+
+//     if(jacketNameInCart.name === productName && jacketNameInCart.color === lastcolor.color && jacketNameInCart.size === lastsize.size){
+
+//         alert("A jacket with the same color and size is already in the shopping cart. Choose something else or change the product in the shopping cart.");
+//     }
+//     else {
+//         const jacketItems = jacketList.find((jacketList) => jacketList.name === productName);
+            
+//         shoppingCart.push({...jacketItems, color: lastcolor.color, size: lastsize.size, productAmount: 1});
+//         console.log("99999",shoppingCart);
+//     }
+
+//     shoppingCartUpdate();
+// }
 
 
 // const editProductAmount = document.querySelector("#productAmount");
